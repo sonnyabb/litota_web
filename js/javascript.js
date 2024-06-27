9adb93d9e32944ff2fbc6d6bc2c4deef93d082d5
sonnyabb <145772109+sonnyabb@users.noreply.github.com>
2024-06-28 02:53:06 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,104 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // бургер
+    document.querySelector(".burg").addEventListener("click", function() {
+
+        var img = document.querySelector(".contacts");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+
+    // 12 марта
+    document.querySelector(".ev1").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev1 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+    // 13 мая
+    document.querySelector(".ev2").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev2 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+    // 8 июня
+    document.querySelector(".ev3").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev3 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+    // 17 июля
+    document.querySelector(".ev4").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev4 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+    // 22 июля
+    document.querySelector(".ev5").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev5 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+    });
+
+    // 28 августа
+    document.querySelector(".ev6").addEventListener("click", function() {
+
+        var img = document.querySelector(".ev6 img");
+        if (img.style.display === "block") {
+            img.style.display = "none";
+        } 
+        
+        else {
+            img.style.display = "block";
+        }
+
+});
+
+});
+
+
